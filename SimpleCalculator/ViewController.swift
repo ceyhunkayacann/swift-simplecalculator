84d0ca4d0ea1b9fbf1885fc1895b9aa872d9a28c
Ceyy <ceyslab@gmail.com>
2023-01-02 13:43:21 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Ceyhun Kayacan on 2.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
